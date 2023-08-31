8606e9093773f11c6c25170c2487c28af0a92128
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2023-08-31 15:57:32 +0800

    Initial Commit

---
+++ cypress/e2e/utilities/sign-up/signup-locators.ts
@@ -0,0 +1,36 @@
+//SIGN-UP FIELD HEADERS
+export const FIRST_LAST_NAME_HDR = '[id=name-label]';
+export const EMAIL_HDR = '[id=email-label]';
+export const MOBILE_NUMBER_HDR = '[id=mobile-label]';
+export const BUSINESS_NAME_HDR = '[id=business_name-label]';
+export const EMPLOYEE_DROPDOWN_HDR= '[id=num_of_employees-label]';
+export const PASSWORD_HDR = '[id=password-label]';
+
+//SIGN-UP FIELD LOCATORS
+export const FIRST_LAST_NAME_FLD = 'input[name="name"]';
+export const EMAIL_FLD = 'input[name="email"]';
+export const MOBILE_NUMBER_FLD = 'input[name="mobile"]';
+export const BUSINESS_NAME_FLD = 'input[name="business_name"]';
+export const EMPLOYEE_DROPDOWN= 'li.indexes__SelectListButtonStyled-sc-1su5p0r-107';
+export const DOWN_EMP_DROPDOWN = '[data-testid="svg_DownChevron"]';
+export const PASSWORD_FLD = 'input[name="password"]';
+
+//SIGN-UP INFO FIELD LOCATORS
+export const MOBILE_NUMBER_INFO_FLD = 'div.sign-up-containerstyled__FormWrapper-sc-1h3enm5-0.jFDtHJ > form > div:nth-child(5)';
+export const PASSWORD_INFO_FLD = 'div.sign-up-containerstyled__FormWrapper-sc-1h3enm5-0.jFDtHJ > form > div:nth-child(10)';
+export const LOADING_SPINNER = '[data-testid="global-spinner"]';
+
+//SIGN-UP ERROR FIELD LOCATORS
+export const FIRST_LAST_NAME_ERROR_FLD = 'div:nth-child(1) > div.indexes__ErrorTextStyled-sc-1su5p0r-8.lnBphi';
+export const EMAIL_ERROR_FLD = 'div:nth-child(2) > div.indexes__ErrorTextStyled-sc-1su5p0r-8.lnBphi';
+export const MOBILE_NUMBER_ERROR_FLD = 'div:nth-child(3) > div.indexes__ErrorTextStyled-sc-1su5p0r-8.lnBphi';
+export const BUSINESS_NAME_ERROR_FLD = 'div:nth-child(6) > div.indexes__ErrorTextStyled-sc-1su5p0r-8.lnBphi';
+export const EMPLOYEE_DROPDOWN_ERROR_FLD = 'div:nth-child(2) > div.indexes__ErrorTextStyled-sc-1su5p0r-8.lnBphi';
+export const PASSWORD_ERROR_FLD = 'div:nth-child(8) > div.indexes__ErrorTextStyled-sc-1su5p0r-8.lnBphi';
+
+//SIGN-UP BUTTONS
+export const LETSGO_BTN = 'button[type="submit"]';
+export const SKIP_ONBOARDING_BTN = '[data-testid="skip-pick-a-time-onboarding"]';
+export const CLOSE_TUTORIAL_BTN = '[data-testid="svg-close"]';
+
+
