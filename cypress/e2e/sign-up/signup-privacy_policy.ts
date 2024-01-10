ea32248f2a9fb7b7369a258d4cb7800b77b5997e
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-10 14:11:57 +0800

    Update signup-privacy_policy.ts

--- cypress/e2e/sign-up/signup-privacy_policy.ts
+++ cypress/e2e/sign-up/signup-privacy_policy.ts
@@ -1,34 +1,113 @@
 import {
-  FIRST_LAST_NAME_HDR,
-  EMAIL_HDR,
-  MOBILE_NUMBER_HDR,
-  BUSINESS_NAME_HDR,
-  EMPLOYEE_DROPDOWN_HDR,
-  PASSWORD_HDR,
-  LETSGO_BTN
+  FIRST_LAST_NAME_FLD,
+  EMAIL_FLD,
+  MOBILE_NUMBER_FLD,
+  BUSINESS_NAME_FLD,
+  EMPLOYEE_DROPDOWN,
+  DOWN_EMP_DROPDOWN,
+  PASSWORD_FLD,
+  LETSGO_BTN,
+  FIRST_LAST_NAME_ERROR_FLD,
+  EMAIL_ERROR_FLD,
+  MOBILE_NUMBER_ERROR_FLD,
+  BUSINESS_NAME_ERROR_FLD,
+  EMPLOYEE_DROPDOWN_ERROR_FLD,
+  PASSWORD_ERROR_FLD,
+  PASSWORD_INFO_FLD,
+  MOBILE_NUMBER_INFO_FLD
 } from '../../utilities/sign-up/signup-locators'
 
-describe('Verify Signup Page is loaded', () => {
-  it('Visits the staging workyard website Signup Page', () => {
+import {
+  FIRST_LAST_NAME_ERROR_MSG1,
+  FIRST_LAST_NAME_ERROR_MSG2,
+  EMAIL_ERROR_MSG,
+  MOBILE_NUMBER_ERROR_MSG1,
+  MOBILE_NUMBER_ERROR_MSG2,
+  BUSINESS_NAME_ERROR_MSG,
+  EMPLOYEE_DROPDOWN_ERROR_MSG,
+  PASSWORD_ERROR_MSG1,
+  PASSWORD_ERROR_MSG2,
+  MOBILE_NUMBER_INFO_MSG,
+  PASSWORD_INFO_MSG,
+  DEFAULT_PASSWORD,
+  INIT_NAME,
+  NEW_SIGNUP_EMAIL,
+  MOBILE_NUMBER_SIGNUP,
+  BUSINESS_NAME_SIGNUP
+} from '../../utilities/sign-up/signup-messages-constants'
+
+describe('Sign-up field validation', () => {
+  it('should allow a user to sign up successfully', () => {
+    // User is navigated to SIGN-UP page
     const baseUrl = Cypress.env('baseUrl')
     cy.visit(`${baseUrl}/sign_up`)
-    cy.contains('Try Workyard for free').should('be.visible')
 
-    // Verify all sign-up fields are displayed
-    cy.get(FIRST_LAST_NAME_HDR).should('be.visible')
-    cy.get(EMAIL_HDR).should('be.visible')
-    cy.get(MOBILE_NUMBER_HDR).should('be.visible')
-    cy.get(BUSINESS_NAME_HDR).should('be.visible')
-    cy.get(EMPLOYEE_DROPDOWN_HDR).should('be.visible')
-    cy.get(PASSWORD_HDR).should('be.visible')
-    cy.get(LETSGO_BTN).scrollIntoView().should('be.visible')
+    // First Round of Error Field Validations
+    cy.get(LETSGO_BTN).click()
+    cy.get(FIRST_LAST_NAME_ERROR_FLD)
+      .should('be.visible')
+      .and('contain', FIRST_LAST_NAME_ERROR_MSG1)
+    cy.get(EMAIL_ERROR_FLD).should('be.visible').and('contain', EMAIL_ERROR_MSG)
+    cy.get(MOBILE_NUMBER_ERROR_FLD)
+      .should('be.visible')
+      .and('contain', MOBILE_NUMBER_ERROR_MSG1)
+    cy.get(BUSINESS_NAME_ERROR_FLD)
+      .should('be.visible')
+      .and('contain', BUSINESS_NAME_ERROR_MSG)
+    cy.get(EMPLOYEE_DROPDOWN_ERROR_FLD)
+      .should('be.visible')
+      .and('contain', EMPLOYEE_DROPDOWN_ERROR_MSG)
+    cy.get(PASSWORD_ERROR_FLD)
+      .should('be.visible')
+      .and('contain', PASSWORD_ERROR_MSG1)
 
-    // Verify that Terms of Use and Provicy Policy has the correct hyperlinks
-    cy.contains('a', 'Terms of Use')
+    // Second Round of Error Field Validations
+    cy.get(FIRST_LAST_NAME_FLD).type('M')
+    cy.get(FIRST_LAST_NAME_ERROR_FLD)
       .should('be.visible')
-      .and('have.attr', 'href', 'https://www.workyard.com/terms')
-    cy.contains('a', 'Privacy Policy')
+      .and('contain', FIRST_LAST_NAME_ERROR_MSG2)
+    cy.get(MOBILE_NUMBER_FLD).type('+1')
+    cy.get(MOBILE_NUMBER_ERROR_FLD)
       .should('be.visible')
-      .and('have.attr', 'href', 'https://www.workyard.com/privacy')
+      .and('contain', MOBILE_NUMBER_ERROR_MSG2)
+    cy.get(PASSWORD_FLD).type('wy')
+    cy.get(PASSWORD_ERROR_FLD)
+      .should('be.visible')
+      .and('contain', PASSWORD_ERROR_MSG2)
+
+    cy.get(FIRST_LAST_NAME_FLD).clear()
+    cy.get(MOBILE_NUMBER_FLD).clear()
+    cy.get(PASSWORD_FLD).clear()
+
+    // Supplying valid inputs to all fields
+    cy.genRandomString(6).then(randomString => {
+      const finName = INIT_NAME + randomString
+      cy.get(FIRST_LAST_NAME_FLD).type(finName)
+    })
+
+    cy.get(EMAIL_FLD).type(NEW_SIGNUP_EMAIL)
+
+    cy.get(MOBILE_NUMBER_FLD).type(MOBILE_NUMBER_SIGNUP)
+
+    cy.get(BUSINESS_NAME_FLD).type(BUSINESS_NAME_SIGNUP)
+
+    cy.get(DOWN_EMP_DROPDOWN).should('be.visible').click()
+    cy.get(EMPLOYEE_DROPDOWN).should('be.visible').contains('6-10').click()
+    cy.get(PASSWORD_FLD).type(DEFAULT_PASSWORD)
+
+    // Third Round of Error Field Validations
+    cy.get(FIRST_LAST_NAME_ERROR_FLD).should('not.exist')
+    cy.get(EMAIL_ERROR_FLD).should('not.exist')
+    cy.get(MOBILE_NUMBER_ERROR_FLD).should('not.exist')
+    cy.get(BUSINESS_NAME_ERROR_FLD).should('not.exist')
+    cy.get(EMPLOYEE_DROPDOWN_ERROR_FLD).should('not.exist')
+
+    // Verifying of Info Messages Displayed
+    cy.get(MOBILE_NUMBER_INFO_FLD)
+      .should('be.visible')
+      .and('contain', MOBILE_NUMBER_INFO_MSG)
+    cy.get(PASSWORD_INFO_FLD)
+      .should('be.visible')
+      .and('contain', PASSWORD_INFO_MSG)
   })
 })
