53c08a521d6536727966d32796874d1e1c5f8b16
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-10 14:11:22 +0800

    Update signup-terms_of_use.ts

--- cypress/e2e/sign-up/signup-terms_of_use.ts
+++ cypress/e2e/sign-up/signup-terms_of_use.ts
@@ -1,25 +1,76 @@
 describe('Verify Workyard Terms of Service page is loaded', () => {
-    it('should visit the Workyard Terms of Service Page', () => {
-    const TERMS_OF_USE_URL = 'https://www.workyard.com/terms';
-  
-      // Visit the webpage for TERMS OF USE
-      cy.visit(TERMS_OF_USE_URL);
-      cy.title().should('contain', 'Terms and Condition')
+  it('should visit the Workyard Terms of Service Page', () => {
+    const TERMS_OF_USE_URL = 'https://www.workyard.com/terms'
 
-      cy.contains('h1', 'Terms of Service').scrollIntoView().should('be.visible').and('contain', "Terms of Service");
-      cy.contains('h3', '1. GENERAL RULES').scrollIntoView().should('be.visible').and('contain', "1. GENERAL RULES");
-      cy.contains('h3', '2. YOUR ACCOUNT').scrollIntoView().should('be.visible').and('contain', "2. YOUR ACCOUNT");
-      cy.contains('h3', '3. PROPRIETARY RIGHTS AND LICENSES').scrollIntoView().should('be.visible').and('contain', "3. PROPRIETARY RIGHTS AND LICENSES");
-      cy.contains('h3', '4. PRIVACY AND DATA').scrollIntoView().should('be.visible').and('contain', "4. PRIVACY AND DATA");
-      cy.contains('h3', '5. TELECOMMUNICATIONS RESPONSIBLIITY').scrollIntoView().should('be.visible').and('contain', "5. TELECOMMUNICATIONS RESPONSIBLIITY");
-      cy.contains('h3', '6. CONTENT').scrollIntoView().should('be.visible').and('contain', "6. CONTENT");
-      cy.contains('h3', '7. SUBSCRIPTIONS').scrollIntoView().should('be.visible').and('contain', "7. SUBSCRIPTIONS");
-      cy.contains('h3', '8. TERM AND TERMINATION').scrollIntoView().should('be.visible').and('contain', "8. TERM AND TERMINATION");
-      cy.contains('h3', '9. WARRANTY DISCLAIMER').scrollIntoView().should('be.visible').and('contain', "9. WARRANTY DISCLAIMER");
-      cy.contains('h3', '10. LIMITATION OF LIABILITY AND INDEMNIFICATION').scrollIntoView().should('be.visible').and('contain', "10. LIMITATION OF LIABILITY AND INDEMNIFICATION");
-      cy.contains('h3', '11. GOVERNING LAW AND DISPUTES').scrollIntoView().should('be.visible').and('contain', "11. GOVERNING LAW AND DISPUTES");
-      cy.contains('h3', '12. MISCELLANEOUS').scrollIntoView().should('be.visible').and('contain', "12. MISCELLANEOUS");
-      cy.contains('p', 'Last updated 8/24/2020').scrollIntoView().should('be.visible').and('contain', "Last updated 8/24/2020");
+    // Visit the webpage for TERMS OF USE
+    cy.visit(TERMS_OF_USE_URL)
+    cy.title().should('contain', 'Terms and Condition')
 
-    });
-  });
+    cy.assertScrollIntoViewElementContainsText(
+      'h1',
+      'Terms of Service',
+      'Terms of Service'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '1. GENERAL RULES',
+      '1. GENERAL RULES'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '2. YOUR ACCOUNT',
+      '2. YOUR ACCOUNT'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '3. PROPRIETARY RIGHTS AND LICENSES',
+      '3. PROPRIETARY RIGHTS AND LICENSES'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '4. PRIVACY AND DATA',
+      '4. PRIVACY AND DATA'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '5. TELECOMMUNICATIONS RESPONSIBLIITY',
+      '5. TELECOMMUNICATIONS RESPONSIBLIITY'
+    )
+    cy.assertScrollIntoViewElementContainsText('h3', '6. CONTENT', '6. CONTENT')
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '7. SUBSCRIPTIONS',
+      '7. SUBSCRIPTIONS'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '8. TERM AND TERMINATION',
+      '8. TERM AND TERMINATION'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '9. WARRANTY DISCLAIMER',
+      '9. WARRANTY DISCLAIMER'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '10. LIMITATION OF LIABILITY AND INDEMNIFICATION',
+      '10. LIMITATION OF LIABILITY AND INDEMNIFICATION'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '11. GOVERNING LAW AND DISPUTES',
+      '11. GOVERNING LAW AND DISPUTES'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '12. MISCELLANEOUS',
+      '12. MISCELLANEOUS'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'p',
+      'Last updated 8/24/2020',
+      'Last updated 8/24/2020'
+    )
+  })
+})
