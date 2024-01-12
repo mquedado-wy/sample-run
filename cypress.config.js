2bb708dd7242ac06441be0cd9fda63428218a10e
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-12 12:10:14 +0800

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,10 @@ module.exports = {
   record: true,
   chromeWebSecurity: false,
   e2e: {
-    specPattern: 'cypress/e2e/**/*.ts',
+    specPattern: 'cypress/e2e/subscription/*.ts',
     excludeSpecPattern: [
       'cypress/utilities/**/*.ts',
-      'cypress/e2e/subscription/*.ts'
+      'cypress/e2e/*/*.ts'
       ]
   }
 }
