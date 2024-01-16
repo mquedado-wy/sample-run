cbb401f8f7cac7843ee850110ec1187c7c8fc10e
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-16 19:02:13 +0800

    Update login-locators.ts

--- cypress/utilities/login/login-locators.ts
+++ cypress/utilities/login/login-locators.ts
@@ -3,13 +3,12 @@ export const WORKYARD_LOGO = 'svg[data-testid="workyard-logo"]'
 export const LOGIN_HDR = 'div[data-testid="login_form_heading"]'
 export const LOGIN_EMAIL_HDR = 'div[data-testid="email-field-heading"]'
 export const LOGIN_PASSWORD_HDR = 'div[data-testid="password-field-heading"]'
-export const LOGIN_EMAIL_FLD = 'input[name="email"]'
-export const LOGIN_PASSWORD_FLD = 'input[name="password"]'
+export const LOGIN_EMAIL_FLD = 'input[data-testid="login-email-field"]'
+export const LOGIN_PASSWORD_FLD = 'input[data-testid="login-password-field"]'
 export const LOGIN_BTN = 'button[data-testid="login_form_submit-btn"]'
-export const LOGIN_FORM_DIVIDER =
-  '[class="LoginForm__Separator-sc-12lybvv-5 imTVZV"]'
+export const LOGIN_FORM_DIVIDER = 'div[data-testid="login_form_divider"]'
 export const RESET_IT_HERE_BTN = 'button[data-testid="login_form_reset-password"]'
-export const LOGIN_WITH_MOBILE = 'button[kind="transparent-black"]'
+export const LOGIN_WITH_MOBILE = 'a[data-testid="login_form_mobile-login"]'
 
 // LOGIN ERROR FIELD LOCATORS
 export const LOGIN_EMAIL_ERROR_FLD = 'div[data-testid="email-error-field"]'
