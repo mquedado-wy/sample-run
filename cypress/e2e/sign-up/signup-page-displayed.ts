35d650e051917811e1633f42fef259525ade9752
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-16 18:04:05 +0800

    Update signup-page-displayed.ts

--- cypress/e2e/sign-up/signup-page-displayed.ts
+++ cypress/e2e/sign-up/signup-page-displayed.ts
@@ -12,23 +12,31 @@ describe('Verify Signup Page is loaded', () => {
   it('Visits the staging workyard website Signup Page', () => {
     const baseUrl = Cypress.env('baseUrl')
     cy.visit(`${baseUrl}/sign_up`)
-    cy.contains('Try Workyard for free').should('be.visible')
+    cy.assertElementContainsText('h1', 'Try Workyard for free')
 
     // Verify all sign-up fields are displayed
-    cy.get(FIRST_LAST_NAME_LBL).should('be.visible')
-    cy.get(EMAIL_LBL).should('be.visible')
-    cy.get(MOBILE_NUMBER_LBL).should('be.visible')
-    cy.get(BUSINESS_NAME_LBL).should('be.visible')
-    cy.get(EMPLOYEE_DROPDOWN_LBL).should('be.visible')
-    cy.get(PASSWORD_LBL).should('be.visible')
-    cy.get(LETSGO_BTN).scrollIntoView().should('be.visible')
+    cy.assertElementsAreVisible([
+      FIRST_LAST_NAME_LBL,
+      EMAIL_LBL,
+      MOBILE_NUMBER_LBL,
+      BUSINESS_NAME_LBL,
+      EMPLOYEE_DROPDOWN_LBL,
+      PASSWORD_LBL
+    ])
+    cy.assertScrollIntoViewElementIsVisible(LETSGO_BTN)
 
     // Verify that Terms of Use and Provicy Policy has the correct hyperlinks
-    cy.contains('a', 'Terms of Use')
-      .should('be.visible')
-      .and('have.attr', 'href', 'https://www.workyard.com/terms')
-    cy.contains('a', 'Privacy Policy')
-      .should('be.visible')
-      .and('have.attr', 'href', 'https://www.workyard.com/privacy')
+    cy.assertScrollIntoViewElementHaveAttr(
+      'a',
+      'Terms of Use',
+      'href',
+      'https://www.workyard.com/terms'
+    )
+    cy.assertScrollIntoViewElementHaveAttr(
+      'a',
+      'Privacy Policy',
+      'href',
+      'https://www.workyard.com/privacy'
+    )
   })
 })
