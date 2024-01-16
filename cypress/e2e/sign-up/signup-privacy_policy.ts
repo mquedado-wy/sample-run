edd07f3b4d3ba0b4e131481849a2b0f281510d87
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-16 20:32:12 +0800

    Update signup-privacy_policy.ts

--- cypress/e2e/sign-up/signup-privacy_policy.ts
+++ cypress/e2e/sign-up/signup-privacy_policy.ts
@@ -1,24 +1,34 @@
 describe('Verify Workyard Privacy Policy page is loaded', () => {
   it('should visit the Workyard Privacy Policy Page', () => {
-    const TERMS_OF_USE_URL = 'https://www.workyard.com/privacy'
+    const PRIVACY_POLICY_URL = 'https://www.workyard.com/privacy'
 
     // Visit the webpage for TERMS OF USE
-    cy.visit(TERMS_OF_USE_URL)
+    cy.visit(PRIVACY_POLICY_URL)
 
     cy.assertScrollIntoViewElementContainsText(
       'h1',
-      'Workyard Privacy Policy',
-      'Workyard Privacy Policy'
+      'Privacy policy',
+      'Privacy policy'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'p',
-      'Last Updated 9/15/2020',
-      'Last Updated 9/15/2020'
+      'div',
+      'Last Updated',
+      'Last Updated'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'div',
+      'Oct 15, 2023',
+      'Oct 15, 2023'
     )
     cy.assertScrollIntoViewElementContainsText(
       'h2',
-      'Purpose of Our Policy',
-      'Purpose of Our Policy'
+      'How You are Subject to this Privacy Policy',
+      'How You are Subject to this Privacy Policy'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'What We Mean by Personal Information',
+      'What We Mean by Personal Information'
     )
     cy.assertScrollIntoViewElementContainsText(
       'h2',
@@ -27,25 +37,89 @@ describe('Verify Workyard Privacy Policy page is loaded', () => {
     )
     cy.assertScrollIntoViewElementContainsText(
       'h2',
-      'How Personal Information Is Collected',
-      'How Personal Information Is Collected'
+      'Usage Data We Collect',
+      'Usage Data We Collect'
     )
     cy.assertScrollIntoViewElementContainsText(
       'h2',
-      'How Personal Information Is Used and Disclosed',
-      'How Personal Information Is Used and Disclosed'
+      'User Content',
+      'User Content'
     )
     cy.assertScrollIntoViewElementContainsText(
       'h2',
-      'Cookies and Other Tracking Technologies',
-      'Cookies and Other Tracking Technologies'
+      'Geolocation Data',
+      'Geolocation Data'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'Personal Information Collected If You Make Purchases',
+      'Personal Information Collected If You Make Purchases'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'How We Use or Share Your Personal Information',
+      'How We Use or Share Your Personal Information'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'When You Should Not Share Personal Information With Us',
+      'When You Should Not Share Personal Information With Us'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'Other Information We Collect and Use (Not Personal)',
+      'Other Information We Collect and Use (Not Personal)'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'What Other Technologies Affect Your Privacy and Use of Our Services',
+      'What Other Technologies Affect Your Privacy and Use of Our Services'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'Promotional and Non-Promotional Emails',
+      'Promotional and Non-Promotional Emails'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'Advertising',
+      'Advertising'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'How User Data is Processed and Stored',
+      'How User Data is Processed and Stored'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'Third Party Services, Devices and Links',
+      'Third Party Services, Devices and Links'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'Third Party Services, Devices and Links',
+      'Third Party Services, Devices and Links'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'Hosting Services Provider and Data Transfers',
+      'Hosting Services Provider and Data Transfers'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'How We Secure and Protect Your Personal Information',
+      'How We Secure and Protect Your Personal Information'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'Your Opt-Out Rights',
+      'Your Opt-Out Rights'
     )
     cy.assertScrollIntoViewElementContainsText(
       'h2',
       'Your Choices',
       'Your Choices'
     )
-    cy.assertScrollIntoViewElementContainsText('h2', 'Security', 'Security')
     cy.assertScrollIntoViewElementContainsText(
       'h2',
       'Your Rights',
@@ -53,44 +127,33 @@ describe('Verify Workyard Privacy Policy page is loaded', () => {
     )
     cy.assertScrollIntoViewElementContainsText(
       'h2',
-      'California Privacy Rights',
-      'California Privacy Rights'
+      'For Users Residing in California',
+      'For Users Residing in California'
     )
     cy.assertScrollIntoViewElementContainsText(
       'h2',
-      'Do Not Track',
-      'Do Not Track'
+      'Children',
+      'Children'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h2',
+      'Changes To This Privacy Policy',
+      'Changes To This Privacy Policy'
     )
     cy.assertScrollIntoViewElementContainsText(
       'h2',
       'Complaints & Disputes',
       'Complaints & Disputes'
     )
-    cy.assertScrollIntoViewElementContainsText('h2', 'Children', 'Children')
     cy.assertScrollIntoViewElementContainsText(
       'h2',
-      'Third-Party Links',
-      'Third-Party Links'
+      'How to Contact Us',
+      'How to Contact Us'
     )
     cy.assertScrollIntoViewElementContainsText(
       'h2',
-      'No Rights Of Third Parties',
-      'No Rights Of Third Parties'
-    )
-    cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'How We Communicate With You',
-      'How We Communicate With You'
-    )
-    cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Contacting Us',
-      'Contacting Us'
-    )
-    cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Changes To This Privacy Policy',
-      'Changes To This Privacy Policy'
+      'CALIFORNIA PRIVACY NOTICE',
+      'CALIFORNIA PRIVACY NOTICE'
     )
   })
 })
