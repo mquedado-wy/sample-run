ba417200af1d250a586c19fe0e125a29ffd0faab
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-16 18:04:27 +0800

    Update signup-privacy_policy.ts

--- cypress/e2e/sign-up/signup-privacy_policy.ts
+++ cypress/e2e/sign-up/signup-privacy_policy.ts
@@ -1,96 +1,165 @@
-describe('Verify Workyard Privacy Policy page is loaded', () => {
-  it('should visit the Workyard Privacy Policy Page', () => {
-    const TERMS_OF_USE_URL = 'https://www.workyard.com/privacy'
+describe('Verify Workyard Terms of Service page is loaded', () => {
+  it('should visit the Workyard Terms of Service Page', () => {
+    const TERMS_OF_USE_URL = 'https://www.workyard.com/terms'
 
     // Visit the webpage for TERMS OF USE
     cy.visit(TERMS_OF_USE_URL)
+    cy.title().should('contain', 'Terms and Condition')
 
     cy.assertScrollIntoViewElementContainsText(
       'h1',
-      'Workyard Privacy Policy',
-      'Workyard Privacy Policy'
+      'Terms of Service',
+      'Terms of Service'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'p',
-      'Last Updated 9/15/2020',
-      'Last Updated 9/15/2020'
+      'div',
+      'Last updated',
+      'Last updated'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Purpose of Our Policy',
-      'Purpose of Our Policy'
+      'div',
+      'Oct 13, 2023',
+      'Oct 13, 2023'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Personal Information We Collect',
-      'Personal Information We Collect'
+      'h3',
+      '1. General Rules',
+      '1. General Rules'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'How Personal Information Is Collected',
-      'How Personal Information Is Collected'
+      'h3',
+      '2. Changes To The Terms',
+      '2. Changes To The Terms'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'How Personal Information Is Used and Disclosed',
-      'How Personal Information Is Used and Disclosed'
+      'h3',
+      '3. Scope Of SAAS Services',
+      '3. Scope Of SAAS Services'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Cookies and Other Tracking Technologies',
-      'Cookies and Other Tracking Technologies'
+      'h3',
+      '4. Your Account',
+      '4. Your Account'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Your Choices',
-      'Your Choices'
-    )
-    cy.assertScrollIntoViewElementContainsText('h2', 'Security', 'Security')
-    cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Your Rights',
-      'Your Rights'
+      'h3',
+      '5. Subscription Plans',
+      '5. Subscription Plans'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'California Privacy Rights',
-      'California Privacy Rights'
+      'h3',
+      '6. Licenses And Use Restrictions',
+      '6. Licenses And Use Restrictions'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Do Not Track',
-      'Do Not Track'
+      'h3',
+      '7. Customer Data',
+      '7. Customer Data'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Complaints & Disputes',
-      'Complaints & Disputes'
-    )
-    cy.assertScrollIntoViewElementContainsText('h2', 'Children', 'Children')
-    cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Third-Party Links',
-      'Third-Party Links'
+      'h3',
+      '8. Workyard And Customer Property‍',
+      '8. Workyard And Customer Property‍'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'No Rights Of Third Parties',
-      'No Rights Of Third Parties'
+      'h3',
+      '9. Customer Obligations And Acknowledgements ‍',
+      '9. Customer Obligations And Acknowledgements ‍'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'How We Communicate With You',
-      'How We Communicate With You'
+      'h3',
+      '10. Unauthorized Access',
+      '10. Unauthorized Access'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Contacting Us',
-      'Contacting Us'
+      'h3',
+      '11. Customer System Obligations',
+      '11. Customer System Obligations'
     )
     cy.assertScrollIntoViewElementContainsText(
-      'h2',
-      'Changes To This Privacy Policy',
-      'Changes To This Privacy Policy'
+      'h3',
+      '12. Workyard Obligations',
+      '12. Workyard Obligations'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '13. Data Security',
+      '13. Data Security'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '14. Changes To The Services And Pricing',
+      '14. Changes To The Services And Pricing'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '15. Term And Termination',
+      '15. Term And Termination'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '16. Copyright And Other Intellectual Property Infringement Claims',
+      '16. Copyright And Other Intellectual Property Infringement Claims'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '17. Confidentiality',
+      '17. Confidentiality'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '18. Indemnification',
+      '18. Indemnification'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '19. Representations And Warranties‍',
+      '19. Representations And Warranties‍'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '20. Disclaimers',
+      '20. Disclaimers'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '21. Limitation of Liability',
+      '21. Limitation of Liability'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '22. California Resident Waiver',
+      '22. California Resident Waiver'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '23. Force Majeure',
+      '23. Force Majeure'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '24. Informal DIspute Procedure',
+      '24. Informal DIspute Procedure'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '25. Arbitration',
+      '25. Arbitration'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '26. Miscellaneous Terms',
+      '26. Miscellaneous Terms'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '27. Workyard Electronic Communications To You',
+      '27. Workyard Electronic Communications To You'
+    )
+    cy.assertScrollIntoViewElementContainsText(
+      'h3',
+      '28. Contact Us',
+      '28. Contact Us'
     )
   })
 })
