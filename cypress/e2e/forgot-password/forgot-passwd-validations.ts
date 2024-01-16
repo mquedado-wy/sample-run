6b20c258649e526f33618f7dc751125d83bfae84
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-16 18:02:40 +0800

    Update forgot-passwd-validations.ts

--- cypress/e2e/forgot-password/forgot-passwd-validations.ts
+++ cypress/e2e/forgot-password/forgot-passwd-validations.ts
@@ -42,10 +42,20 @@ describe('Verify Users use "Forgot Password" feature', () => {
     cy.assertElementsAreVisible([WORKYARD_LOGO])
 
     // Verify success message when existing user tries to use forgot password feature
-    cy.assertElementVisibleAndType(FORGOT_PASSWD_EMAIL_FLD, 'fp-email-field', EXISTING_USER_EMAIL)
+    cy.assertElementVisibleAndType(
+      FORGOT_PASSWD_EMAIL_FLD,
+      'fp-email-field',
+      EXISTING_USER_EMAIL
+    )
     cy.assertElementVisibleAndClick(FORGOT_PASSWD_BTN, 'fp-button')
-    cy.assertElementsAreVisible([FORGOT_PASSWD_SUCCESS_LOGO, FORGOT_PASSWD_SUCCESS_FLD])
-    cy.assertElementContainsText(FORGOT_PASSWD_SUCCESS_FLD, FORGOT_PASSWD_SUCCESS_MSG)
+    cy.assertElementsAreVisible([
+      FORGOT_PASSWD_SUCCESS_LOGO,
+      FORGOT_PASSWD_SUCCESS_FLD
+    ])
+    cy.assertElementContainsText(
+      FORGOT_PASSWD_SUCCESS_FLD,
+      FORGOT_PASSWD_SUCCESS_MSG
+    )
   })
 
   it('should not allow non-existent users to use the forgot password feature', () => {
@@ -53,9 +63,16 @@ describe('Verify Users use "Forgot Password" feature', () => {
     cy.assertElementsAreVisible([WORKYARD_LOGO])
 
     // Verify error message when unknown user tries to use forgot password feature
-    cy.assertElementVisibleAndType(FORGOT_PASSWD_EMAIL_FLD, 'fp-email-field', UNKNOWN_USER_EMAIL)
+    cy.assertElementVisibleAndType(
+      FORGOT_PASSWD_EMAIL_FLD,
+      'fp-email-field',
+      UNKNOWN_USER_EMAIL
+    )
     cy.assertElementVisibleAndClick(FORGOT_PASSWD_BTN, 'fp-button')
-    cy.assertElementContainsText(FORGOT_PASSWD_ERROR_FLD, FORGOT_PASSWD_NO_ACCOUNT_MSG)
+    cy.assertElementContainsText(
+      FORGOT_PASSWD_ERROR_FLD,
+      FORGOT_PASSWD_NO_ACCOUNT_MSG
+    )
   })
 
   it('should allow pending users to use the forgot password feature', () => {
@@ -63,9 +80,16 @@ describe('Verify Users use "Forgot Password" feature', () => {
     cy.assertElementsAreVisible([WORKYARD_LOGO])
 
     // Verify info message when pending user tries to use forgot password feature
-    cy.assertElementVisibleAndType(FORGOT_PASSWD_EMAIL_FLD, 'fp-email-field', PENDING_USER_EMAIL)
+    cy.assertElementVisibleAndType(
+      FORGOT_PASSWD_EMAIL_FLD,
+      'fp-email-field',
+      PENDING_USER_EMAIL
+    )
     cy.assertElementVisibleAndClick(FORGOT_PASSWD_BTN, 'fp-button')
-    cy.assertElementContainsText(FORGOT_PASSWD_ERROR_FLD, FORGOT_PASSWD_PENDING_MSG)
+    cy.assertElementContainsText(
+      FORGOT_PASSWD_ERROR_FLD,
+      FORGOT_PASSWD_PENDING_MSG
+    )
   })
 
   it('should show other in-line error messages for invalid inputs', () => {
@@ -74,11 +98,25 @@ describe('Verify Users use "Forgot Password" feature', () => {
 
     // Verify info message when unknown user tries to use forgot password feature
     cy.assertElementVisibleAndClick(FORGOT_PASSWD_BTN, 'fp-button')
-    cy.assertElementContainsText(FORGOT_PASSWD_INLINE_ERROR_FLD, FORGOT_PASSWD_NO_INPUT_MSG)
-    cy.assertElementVisibleAndType(FORGOT_PASSWD_EMAIL_FLD, 'fp-email-field', INVALID_EMAIL)
-    cy.assertElementContainsText(FORGOT_PASSWD_INLINE_ERROR_FLD, FORGOT_PASSWD_INVALID_MSG)
+    cy.assertElementContainsText(
+      FORGOT_PASSWD_INLINE_ERROR_FLD,
+      FORGOT_PASSWD_NO_INPUT_MSG
+    )
+    cy.assertElementVisibleAndType(
+      FORGOT_PASSWD_EMAIL_FLD,
+      'fp-email-field',
+      INVALID_EMAIL
+    )
+    cy.assertElementContainsText(
+      FORGOT_PASSWD_INLINE_ERROR_FLD,
+      FORGOT_PASSWD_INVALID_MSG
+    )
     cy.get(FORGOT_PASSWD_EMAIL_FLD).clear()
-    cy.assertElementVisibleAndType(FORGOT_PASSWD_EMAIL_FLD, 'fp-email-field', EXISTING_USER_EMAIL)
+    cy.assertElementVisibleAndType(
+      FORGOT_PASSWD_EMAIL_FLD,
+      'fp-email-field',
+      EXISTING_USER_EMAIL
+    )
     cy.assertElementsDoNotExist([FORGOT_PASSWD_INLINE_ERROR_FLD])
   })
 
