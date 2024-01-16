54fa3181d9b698b58015724bd5a73265ecef0aec
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-16 17:59:12 +0800

    Update forgot-passwd-validations.ts

--- cypress/e2e/forgot-password/forgot-passwd-validations.ts
+++ cypress/e2e/forgot-password/forgot-passwd-validations.ts
@@ -19,7 +19,7 @@ import {
   FORGOT_PASSWD_NO_ACCOUNT_MSG,
   FORGOT_PASSWD_PENDING_MSG,
   FORGOT_PASSWD_NO_INPUT_MSG,
-  FORGOT_PASSWD_INVALID_MSG,
+  FORGOT_PASSWD_INVALID_MSG
 } from '../../utilities/forgot-password/forgot-passwd-messages-constants'
 
 const baseUrl = Cypress.env('baseUrl')
@@ -39,16 +39,13 @@ describe('Verify Users use "Forgot Password" feature', () => {
 
   it('should display instructions for forgot password', () => {
     cy.visit(`${baseUrl}/forgot_password`)
-    cy.get(WORKYARD_LOGO).should('be.visible')
+    cy.assertElementsAreVisible([WORKYARD_LOGO])
 
     // Verify success message when existing user tries to use forgot password feature
-    cy.get(FORGOT_PASSWD_EMAIL_FLD).should('be.visible').type(EXISTING_USER_EMAIL)
-    cy.get(FORGOT_PASSWD_BTN).should('be.visible').click()
-    cy.get(FORGOT_PASSWD_SUCCESS_LOGO).should('be.visible')
-    cy.get(FORGOT_PASSWD_SUCCESS_FLD).should('be.visible')
-    cy.get(FORGOT_PASSWD_SUCCESS_FLD)
-      .should('be.visible')
-      .contains(FORGOT_PASSWD_SUCCESS_MSG)
+    cy.assertElementVisibleAndType(FORGOT_PASSWD_EMAIL_FLD, 'fp-email-field', EXISTING_USER_EMAIL)
+    cy.assertElementVisibleAndClick(FORGOT_PASSWD_BTN, 'fp-button')
+    cy.assertElementsAreVisible([FORGOT_PASSWD_SUCCESS_LOGO, FORGOT_PASSWD_SUCCESS_FLD])
+    cy.assertElementContainsText(FORGOT_PASSWD_SUCCESS_FLD, FORGOT_PASSWD_SUCCESS_MSG)
   })
 
   it('should not allow non-existent users to use the forgot password feature', () => {
@@ -56,24 +53,19 @@ describe('Verify Users use "Forgot Password" feature', () => {
     cy.assertElementsAreVisible([WORKYARD_LOGO])
 
     // Verify error message when unknown user tries to use forgot password feature
-    cy.get(FORGOT_PASSWD_EMAIL_FLD).should('be.visible').type(UNKNOWN_USER_EMAIL)
-    cy.get(FORGOT_PASSWD_BTN).should('be.visible').click()
-    cy.get(FORGOT_PASSWD_ERROR_FLD).should('be.visible').as('forgot-passwd_fld')
-    cy.get('@forgot-passwd_fld').contains(FORGOT_PASSWD_NO_ACCOUNT_MSG)
+    cy.assertElementVisibleAndType(FORGOT_PASSWD_EMAIL_FLD, 'fp-email-field', UNKNOWN_USER_EMAIL)
+    cy.assertElementVisibleAndClick(FORGOT_PASSWD_BTN, 'fp-button')
+    cy.assertElementContainsText(FORGOT_PASSWD_ERROR_FLD, FORGOT_PASSWD_NO_ACCOUNT_MSG)
   })
 
   it('should allow pending users to use the forgot password feature', () => {
     cy.visit(`${baseUrl}/forgot_password`)
-    cy.get(WORKYARD_LOGO).should('be.visible')
+    cy.assertElementsAreVisible([WORKYARD_LOGO])
 
     // Verify info message when pending user tries to use forgot password feature
-    cy.get(FORGOT_PASSWD_EMAIL_FLD)
-      .should('be.visible')
-      .type(PENDING_USER_EMAIL)
-    cy.get(FORGOT_PASSWD_BTN).should('be.visible').click()
-    cy.get(FORGOT_PASSWD_ERROR_FLD)
-      .should('be.visible')
-      .contains(FORGOT_PASSWD_PENDING_MSG)
+    cy.assertElementVisibleAndType(FORGOT_PASSWD_EMAIL_FLD, 'fp-email-field', PENDING_USER_EMAIL)
+    cy.assertElementVisibleAndClick(FORGOT_PASSWD_BTN, 'fp-button')
+    cy.assertElementContainsText(FORGOT_PASSWD_ERROR_FLD, FORGOT_PASSWD_PENDING_MSG)
   })
 
   it('should show other in-line error messages for invalid inputs', () => {
@@ -81,17 +73,13 @@ describe('Verify Users use "Forgot Password" feature', () => {
     cy.assertElementsAreVisible([WORKYARD_LOGO])
 
     // Verify info message when unknown user tries to use forgot password feature
-    cy.get(FORGOT_PASSWD_BTN).click()
-    cy.get(FORGOT_PASSWD_INLINE_ERROR_FLD)
-      .should('be.visible')
-      .contains(FORGOT_PASSWD_NO_INPUT_MSG)
-    cy.get(FORGOT_PASSWD_EMAIL_FLD).should('be.visible').type(INVALID_EMAIL)
-    cy.get(FORGOT_PASSWD_INLINE_ERROR_FLD)
-      .should('be.visible')
-      .contains(FORGOT_PASSWD_INVALID_MSG)
+    cy.assertElementVisibleAndClick(FORGOT_PASSWD_BTN, 'fp-button')
+    cy.assertElementContainsText(FORGOT_PASSWD_INLINE_ERROR_FLD, FORGOT_PASSWD_NO_INPUT_MSG)
+    cy.assertElementVisibleAndType(FORGOT_PASSWD_EMAIL_FLD, 'fp-email-field', INVALID_EMAIL)
+    cy.assertElementContainsText(FORGOT_PASSWD_INLINE_ERROR_FLD, FORGOT_PASSWD_INVALID_MSG)
     cy.get(FORGOT_PASSWD_EMAIL_FLD).clear()
-    cy.get(FORGOT_PASSWD_EMAIL_FLD).should('be.visible').type(EXISTING_USER_EMAIL)
-    cy.get(FORGOT_PASSWD_INLINE_ERROR_FLD).should('not.exist')
+    cy.assertElementVisibleAndType(FORGOT_PASSWD_EMAIL_FLD, 'fp-email-field', EXISTING_USER_EMAIL)
+    cy.assertElementsDoNotExist([FORGOT_PASSWD_INLINE_ERROR_FLD])
   })
 
   // TODO: Add test for the case where an existing user tries to use forgot password feature
