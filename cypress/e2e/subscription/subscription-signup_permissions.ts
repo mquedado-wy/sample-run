80b56535dbc820b6ecf21edc062877b581364f15
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-19 13:10:52 +0800

    Update subscription-signup_permissions.ts

--- cypress/e2e/subscription/subscription-signup_permissions.ts
+++ cypress/e2e/subscription/subscription-signup_permissions.ts
@@ -145,18 +145,8 @@ describe('Verify the permsissions of Admin, Manager and Worker in Signing up for
     cy.assertElementNotContainText(PERSONAL_BTN, 'Organization')
     cy.assertElementContainsText(PERSONAL_BTN, 'Personal')
   })
-})
-
-describe('DELETE delete_test_orgs', () => {
-  it('successfully deletes test organizations', () => {
-    cy.request({
-      method: 'DELETE',
-      url: 'https://staging-api1.workyard.com/delete_test_orgs',
-      headers: {
-        'x-workyard-system-tests': true
-      }
-    }).then(response => {
-      expect(response.status).to.equal(200)
-    })
+  after(() => {
+    // This is to clean-up the test organization and its related data associated with it
+    cy.testDataCleanUp()
   })
 })
