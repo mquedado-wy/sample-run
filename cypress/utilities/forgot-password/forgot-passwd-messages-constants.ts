2ca380713bf641212b21a79f70ee576198a663f4
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-25 14:08:06 +0800

    Update forgot-passwd-messages-constants.ts

--- cypress/utilities/forgot-password/forgot-passwd-messages-constants.ts
+++ cypress/utilities/forgot-password/forgot-passwd-messages-constants.ts
@@ -7,9 +7,12 @@ export const FORGOT_PASSWD_NO_INPUT_MSG = 'This field is required'
 export const FORGOT_PASSWD_INVALID_MSG = 'Email is not valid.'
 export const FORGOT_PASSWD_PENDING_MSG =
   'This account has a pending invite to an organization. An email has been re-sent which contains a link to join the organization.'
-export const FORGOT_PASSWD_EMAIL_CONTENT_HEADER = 'Reset your password'
-export const FORGOT_PASSWD_EMAIL_CONTENT_MESSAGE = 'Click on the button below to reset your password for your Workyard account.'
-export const FORGOT_PASSWD_EMAIL_CONTENT_WARNING = 'If you did not make this request, please ignore this email and your password will remain the same.'
+export const FP_EMAIL_EXISTINGUSER_CONTENT_HEADER = 'Reset your password'
+export const FP_EMAIL_EXISTINGUSER_CONTENT_MESSAGE = 'Click on the button below to reset your password for your Workyard account.'
+export const FP_EMAIL_EXISTINGUSER_CONTENT_WARNING = 'If you did not make this request, please ignore this email and your password will remain the same.'
+export const FP_EMAIL_PENDINGUSER_CONTENT_HEADER = 'Hi Pending,'
+export const FP_EMAIL_PENDINGUSER_CONTENT_MESSAGE = 'Mark Quedado invited you to join MQ Inc. on Workyard'
+export const FP_EMAIL_PENDINGUSER_CONTENT_INFO = 'Workyard Crew enables you to easily receive and approve time cards.'
 
 // FORGOT PASSWORD CONSTANTS
 export const UNKNOWN_USER_EMAIL = `unknown.user.${Math.floor(Math.random() * 900) + 100}${Cypress.env('appEnv')}@workyard.testinator.com`
