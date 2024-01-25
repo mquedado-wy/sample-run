a5bf45ba1c281371c4a4d734499f81f7f918db0f
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-01-25 11:49:53 +0800

    Update forgot-passwd-email.ts

--- cypress/e2e/forgot-password/forgot-passwd-email.ts
+++ cypress/e2e/forgot-password/forgot-passwd-email.ts
@@ -1,104 +1,212 @@
-import {
-  FORGOT_PASSWD_EMAIL_CONTENT_HEADER,
-  FORGOT_PASSWD_EMAIL_CONTENT_MESSAGE,
-  FORGOT_PASSWD_EMAIL_CONTENT_WARNING
-} from '../../utilities/forgot-password/forgot-passwd-messages-constants'
+declare namespace Cypress {
+  interface Chainable {
+    genRandomString(length: number): Chainable<string>
+    genRandomNumber(length: number): Chainable<string>
+    getStripeElement(fieldName: string): Chainable<JQuery<HTMLElement>>
+    assertElementsDoNotExist(selectors: string[]): Chainable
+    assertElementsAreVisible(selectors: string[]): Chainable
+    assertElementContainsText(selector: string, text: string): Chainable
+    assertElementContainsTextAndClick(selector: string, text: string): Chainable
+    assertScrollIntoViewElementContainsText(
+      selector: string,
+      text: string,
+      expectedText: string
+    ): Chainable
+    assertElementContainsText(selector: string, text: string): Chainable
+    assertScrollIntoViewElementHaveAttr(
+      selector: string,
+      text: string,
+      expectedAttr: string,
+      expectedAttrValue: string
+    ): Chainable
+    assertScrollIntoViewElementIsVisible(selector: string): Chainable
+    assertElementVisibleAndClick(selector: string, aliasName: string): Chainable
+    assertElementVisibleAndType(
+      selector: string,
+      aliasName: string,
+      userInputnput: string
+    ): Chainable
+    assertStringsInElement(
+      elementSelector: string,
+      expectedStrings: string[]
+    ): Chainable<Element>
+    assertElementNotContainText(selector: string, text: string): Chainable
+    assertScrollIntoViewElementsAreVisible(selectors: string[]): Chainable
+    testDataCleanUp(): Chainable
+    mailinatorEmailCleanUp(): Chainable
+    mailinatorDeleteLatestEmail(): Chainable
+  }
+}
+Cypress.Commands.add('genRandomString', (length: number) => {
+  let resultChar = ''
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const charactersLength = characters.length
 
-const testForgotPWUser = Cypress.env('testForgotPWUser')
-const forgotPWAuthToken = Cypress.env('forgotPWAuthToken')
-const mailinatorBearerToken = Cypress.env('mailinatorBearerToken')
-const expectedContentResetPasswordEmail = [
-  FORGOT_PASSWD_EMAIL_CONTENT_HEADER,
-  FORGOT_PASSWD_EMAIL_CONTENT_MESSAGE,
-  FORGOT_PASSWD_EMAIL_CONTENT_WARNING
-]
+  for (let i = 0; i < length; i++) {
+    resultChar += characters.charAt(
+      Math.floor(Math.random() * charactersLength)
+    )
+  }
 
-describe('User Requests for Password Reset - Forgot Password', () => {
-  it('should successfully request for password reset', () => {
+  return cy.wrap(resultChar)
+})
+
+Cypress.Commands.add('genRandomNumber', (length: number) => {
+  let resultNum = ''
+  const numbers = '123456789'
+  const numbersLength = numbers.length
+
+  for (let i = 0; i < length; i++) {
+    resultNum += numbers.charAt(Math.floor(Math.random() * numbersLength))
+  }
+
+  return cy.wrap(resultNum)
+})
+
+Cypress.Commands.add('getStripeElement', (fieldName: string) => {
+  if (Cypress.config('chromeWebSecurity')) {
+    throw new Error(
+      'To get stripe element `chromeWebSecurity` must be disabled'
+    )
+  }
+
+  const selector = `input[name="${fieldName}"]`
+
+  return cy
+    .get('iframe')
+    .its('0.contentDocument.body')
+    .should('not.be.empty')
+    .then(cy.wrap)
+    .find(selector)
+})
+
+Cypress.Commands.add('assertElementsDoNotExist', (selectors: string[]) => {
+  selectors.forEach(selector => {
+    cy.get(selector).should('not.exist')
+  })
+})
+
+Cypress.Commands.add('assertElementsAreVisible', (selectors: string[]) => {
+  selectors.forEach(selector => {
+    cy.get(selector).should('be.visible')
+  })
+})
+
+Cypress.Commands.add(
+  'assertElementContainsText',
+  (selector: string, text: string) => {
+    cy.get(selector).should('be.visible').and('contain', text)
+  }
+)
+
+Cypress.Commands.add(
+  'assertElementContainsTextAndClick',
+  (selector: string, text: string) => {
+    cy.get(selector).should('be.visible').contains(text).click()
+  }
+)
+
+Cypress.Commands.add(
+  'assertElementVisibleAndClick',
+  (selector: string, aliasName: string) => {
+    cy.get(selector).should('be.visible').as(aliasName).click()
+  }
+)
+
+Cypress.Commands.add(
+  'assertElementVisibleAndType',
+  (selector: string, aliasName: string, userInput: string) => {
+    cy.get(selector)
+      .should('be.visible')
+      .as(aliasName)
+      .type(userInput)
+  }
+)
+
+Cypress.Commands.add('assertStringsInElement', (selector, expectedStrings) => {
+  cy.get(selector)
+    .find('li')
+    .each(($option, index) => {
+      cy.wrap($option).invoke('text').should('eq', expectedStrings[index])
+    })
+})
+
+Cypress.Commands.add(
+  'assertElementNotContainText',
+  (selector: string, text: string) => {
+    cy.get(selector)
+      .should('be.visible')
+      .should($element => {
+        const elementText = $element.text()
+        expect(elementText.includes(text)).to.equal(false)
+      })
+  }
+)
+
+Cypress.Commands.add(
+  'assertScrollIntoViewElementContainsText',
+  (selector: string, text: string, expectedText: string) => {
+    cy.contains(selector, text)
+      .scrollIntoView()
+      .should('be.visible')
+      .and('contain', expectedText)
+  }
+)
+
+Cypress.Commands.add(
+  'assertScrollIntoViewElementHaveAttr',
+  (
+    selector: string,
+    text: string,
+    expectedAttr: string,
+    expectedAttrValue: string
+  ) => {
+    cy.contains(selector, text)
+      .scrollIntoView()
+      .should('be.visible')
+      .and('have.attr', expectedAttr, expectedAttrValue)
+  }
+)
+
+Cypress.Commands.add(
+  'assertScrollIntoViewElementIsVisible',
+  (selector: string) => {
+    cy.get(selector).scrollIntoView().should('be.visible')
+  }
+)
+
+Cypress.Commands.add(
+  'assertScrollIntoViewElementsAreVisible',
+  (selectors: string[]) => {
+    selectors.forEach(selector => {
+      cy.get(selector).scrollIntoView().should('be.visible')
+    })
+  }
+)
+Cypress.Commands.add(
+  'testDataCleanUp', () => {
     cy.request({
-      method: 'POST',
-      url: 'https://staging-api1.workyard.com/request_password_reset',
+      method: 'DELETE',
+      url: `https://${Cypress.env('appEnv')}-api1.workyard.com/delete_test_orgs`,
       headers: {
-        Authorization: forgotPWAuthToken,
-        'Workyard-Agent': 'website|Windows|NA|14.5.1|1920|1080|1|NA|Asia/Manila|v:1704844800',
         'x-workyard-system-tests': true
-      },
-      body: {
-        email: testForgotPWUser
       }
     }).then(response => {
-      // Verify that the response status is 200 OK
       expect(response.status).to.equal(200)
     })
-  })
-})
+  }
+)
 
-describe('Verify the contents of the forgot password email', () => {
-  it('should make a successful GET request', () => {
+Cypress.Commands.add(
+  'mailinatorEmailCleanUp', () => {
     cy.request({
-      method: 'GET',
-      url: 'https://mailinator.com/api/v2/domains/private/inboxes?limit=1&sort=descending',
+      method: 'DELETE',
+      url: 'https://mailinator.com/api/v2/domains/private/inboxes',
       headers: {
-        Authorization: mailinatorBearerToken
+        Authorization: `${Cypress.env('mailinatorBearerToken')}`
       }
-    }).then((response) => {
-      // Verify that the response status is 200 OK
+    }).then(response => {
       expect(response.status).to.equal(200)
-      expect(response.body.msgs[0]).to.have.property('id')
-      const msgId = response.body.msgs[0].id
-      const msgTo = response.body.msgs[0].to
-      const msgSubject = response.body.msgs[0].subject
-      cy.wrap(msgId).as('msgId')
-      cy.wrap(msgTo).as('msgTo')
-      // Initial Check if the email was sent to the correct user
-      expect(testForgotPWUser).to.have.contain(msgTo)
-      cy.wrap(msgSubject).as('msgSubject')
-      // Initial Check if the email has the correct subject for forgot password
-      expect(msgSubject).to.contain('Workyard Password Reset')
-
-      cy.request({
-        method: 'GET',
-        url: `https://mailinator.com/api/v2/domains/private/messages/${msgId}`
-      // Additional request details
-      }).then((response) => {
-        // Verify the body.headers responses
-        expect(response.body.headers).to.have.property('x-mailgun-template-name', 'password_reset')
-        // Verify the sender of the emeail is correct
-        expect(response.body.headers).to.have.property('sender', 'hello@mail.workyard.com')
-        // Verify if the email has the correct subject for forgot password
-        expect(response.body.headers).to.have.property('subject', 'Workyard Password Reset')
-        // Verify if the forgot password was sent to correct user
-        expect(response.body.headers).to.have.property('to', testForgotPWUser)
-
-        // Verify the body responses
-        // Verify that the email was from 'Workyard'
-        expect(response.body).to.have.property('from', 'Workyard')
-        // Verify the sender of the emeail is correct
-        expect(response.body).to.have.property('origfrom', 'Workyard <hello@mail.workyard.com>')
-        // Verify if the forgot password was sent to correct user
-        expect(response.body).to.have.property('to', msgTo)
-        // Verify if the msgId matches from the request
-        expect(response.body).to.have.property('id', msgId)
-        // expect(response.body).to.have.property('time', 1705896579000)
-        expect(response.body).to.have.property('seconds_ago').to.be.a('number')
-
-        // Verify the content of the reset password email
-        expectedContentResetPasswordEmail.forEach(expectedContentResetPasswordEmail => {
-          expect(response.body.parts[0].body).to.include(expectedContentResetPasswordEmail)
-        })
-      })
-      cy.request({
-        method: 'GET',
-        url: `https://mailinator.com/api/v2/domains/private/messages/${msgId}/links`,
-        headers: {
-          Authorization: mailinatorBearerToken
-        }
-      }).then((response) => {
-        // Verify that the response status is 200 OK
-        expect(response.status).to.equal(200)
-        // Verify that the reset password hyperlink contains the correct staging url
-        expect(response.body.links[0]).to.have.contains('https://www.stagingworkyardwebsite.com/u/crew_app/reset_password/')
-      })
     })
-  })
-})
+  }
+)
