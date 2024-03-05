80005965c350bbf423c91dc00a8c6c2c1b6f1be5
mquedado-wy <142284304+mquedado-wy@users.noreply.github.com>
2024-03-05 16:44:45 +0800

    Update subscription-locators.ts

--- cypress/utilities/subscription/subscription-locators.ts
+++ cypress/utilities/subscription/subscription-locators.ts
@@ -1,5 +1,5 @@
 // SUBSCRIPTON LOCATORS
-export const SUBSCRIPTION_MODAL_CONTAINER = 'div[data-testid="modal-container"]'
+export const SUBSCRIBE_REACTIVATE_CONTAINER = 'div.k-widget.k-window.k-dialog > div'
 export const PLAN_DROPDOWN = 'span[data-testid="desktop-label"]'
 export const PLAN_DROPDOWN_LIST = 'li[class="indexes__SelectListButtonStyled-sc-1su5p0r-107 hKAkBg"]'
 export const PLAN_DROPDOWN_OPTIONS = 'ul[data-testid="desktop-option-list"]'
@@ -38,11 +38,21 @@ export const UNLOCK_FLD = 'div[data-testid="wfm-trial-banner"]'
 export const START_30_DAY_WFM_TRIAL_BTN = 'button[data-testid="start-free-trial-banner-btn"]'
 
 // TRIAL EXPIRED LOCATORS
-export const TRIAL_EXPIRED_FLD1 = 'div[data-testid="billing-modal_header"]'
-export const TRIAL_EXPIRED_FLD2 = 'div[data-testid="billing-modal_sub-header"]'
-export const TRIAL_EXPIRED_DIV_OVERLAY = 'div[data-testid="modal-overlay"]'
+export const TRIAL_EXPIRED_FLD1 = 'div[data-testid="reactivate-trial-modal_inactive-trial-heading"]'
+export const TRIAL_EXPIRED_FLD2 = 'div[data-testid="reactivate-trial-modal_inactive-trial-description"]'
+export const TRIAL_EXPIRED_DIV_OVERLAY = 'div[data-testid="reactivate-trial-modal_inactive-trial-heading"]'
 export const TRIAL_EXPIRED_DAYS_LEFT = 'strong[data-testid="tt-trial-days-left"]'
 export const TRIAL_EXPIRED_DAYS_LEFT2 = 'div[data-testid="tt-trial-days-left-container"]'
 export const TRIAL_EXPIRED_WANT_HELP = 'div[data-testid="tt-banner-want-help"]'
 export const TRIAL_EXPIRED_ACTIVATE_BTN = 'button[data-testid="tt-banner-activate-btn"]'
 export const CURRENT_VALUE_OF_PLAN_TYPE = 'div[data-testid="field-element"]'
+export const TRIAL_EXPIRED_REACTIVATE_OPTION = 'div[data-testid="reactivate-trial-modal_option-reactivate"]'
+export const TRIAL_EXPIRED_REACTIVATE_FIELD_A = 'div:nth-child(1) > span > span.reactivate-trial-modalstyled__OptionTitle-sc-1yjjaap-4.cxxSa'
+export const TRIAL_EXPIRED_REACTIVATE_FIELD_B = 'div.reactivate-trial-modalstyled__CardWrapper-sc-1yjjaap-0.hHbnWh > div:nth-child(1) > span > span:nth-child(2)'
+export const TRIAL_EXPIRED_SUBSCRIBE_OPTION = 'div[data-testid="reactivate-trial-modal_option-subscribe"]'
+export const TRIAL_EXPIRED_SUBSCRIBE_FIELD_A = 'div:nth-child(2) > span > span.reactivate-trial-modalstyled__OptionTitle-sc-1yjjaap-4.cxxSa'
+export const TRIAL_EXPIRED_SUBSCRIBE_FIELD_B = '#reactivate-trial-modal-option-subscribe > span > span:nth-child(2)'
+
+// TRIAL EXPIRED BUTTONS
+export const TRIAL_EXPIRED_CANCEL_BUTTON = 'button[data-testid="reactivate-trial-modal_button-cancel"]'
+export const TRIAL_EXPIRED_CONFIRM_BUTTON = 'data-testid="reactivate-trial-modal_button-submit"'
